67265c796ece7d26a11a523c6229fd18c98af6f5
Eugene Vyshnytsky <101946519+Vishnyak13@users.noreply.github.com>
2022-11-20 20:59:22 +0200

    add try/except/finally for insert_data

--- insert_db.py
+++ insert_db.py
@@ -14,36 +14,42 @@ fake = Faker('uk_UA')
 
 
 def insert_data():
-    with sqlite3.connect('education.db') as connection:
-        cursor = connection.cursor()
+    try:
+        with sqlite3.connect('education.db') as connection:
+            cursor = connection.cursor()
 
-        sql_classes = 'INSERT INTO classes (class_name) VALUES (?)'
-        for _ in range(CLASSES):
-            cursor.execute(sql_classes, (fake.bothify(text='Class: ?-##', letters='АБВГ'),))
-        class_id = [i[0] for i in cursor.execute('SELECT id FROM classes')]
+            sql_classes = 'INSERT INTO classes (class_name) VALUES (?)'
+            for _ in range(CLASSES):
+                cursor.execute(sql_classes, (fake.bothify(text='Class: ?-##', letters='АБВГ'),))
+            class_id = [i[0] for i in cursor.execute('SELECT id FROM classes')]
 
-        sql_students = 'INSERT INTO students (student_name, class_id) VALUES (?, ?)'
-        for _ in range(STUDENTS):
-            cursor.execute(sql_students, (fake.first_name() + ' ' + fake.last_name(), choice(class_id),))
-        student_id = [i[0] for i in cursor.execute('SELECT id FROM students')]
+            sql_students = 'INSERT INTO students (student_name, class_id) VALUES (?, ?)'
+            for _ in range(STUDENTS):
+                cursor.execute(sql_students, (fake.first_name() + ' ' + fake.last_name(), choice(class_id),))
+            student_id = [i[0] for i in cursor.execute('SELECT id FROM students')]
 
-        sql_teachers = 'INSERT INTO teachers (teacher_name) VALUES (?)'
-        for _ in range(TEACHERS):
-            cursor.execute(sql_teachers, (fake.first_name() + ' ' + fake.last_name(),))
-        teacher_id = [i[0] for i in cursor.execute('SELECT id FROM teachers')]
+            sql_teachers = 'INSERT INTO teachers (teacher_name) VALUES (?)'
+            for _ in range(TEACHERS):
+                cursor.execute(sql_teachers, (fake.first_name() + ' ' + fake.last_name(),))
+            teacher_id = [i[0] for i in cursor.execute('SELECT id FROM teachers')]
 
-        sql_disciplines = 'INSERT INTO disciplines (discipline_name, teacher_id) VALUES (?, ?)'
-        for discipline in DISCIPLINES:
-            cursor.execute(sql_disciplines, (discipline, choice(teacher_id),))
-        discipline_id = [i[0] for i in cursor.execute('SELECT id FROM disciplines')]
+            sql_disciplines = 'INSERT INTO disciplines (discipline_name, teacher_id) VALUES (?, ?)'
+            for discipline in DISCIPLINES:
+                cursor.execute(sql_disciplines, (discipline, choice(teacher_id),))
+            discipline_id = [i[0] for i in cursor.execute('SELECT id FROM disciplines')]
 
-        sql_marks = 'INSERT INTO marks (mark, mark_date, student_id, discipline_id) VALUES (?, ?, ?, ?)'
-        for student in student_id:
-            for discipline in discipline_id:
-                for _ in range(MARKS):
-                    cursor.execute(sql_marks, (randint(4, 12), fake.date_this_year(), student, discipline,))
+            sql_marks = 'INSERT INTO marks (mark, mark_date, student_id, discipline_id) VALUES (?, ?, ?, ?)'
+            for student in student_id:
+                for discipline in discipline_id:
+                    for _ in range(MARKS):
+                        cursor.execute(sql_marks, (randint(4, 12), fake.date_this_year(), student, discipline,))
 
-        connection.commit()
+            connection.commit()
+    except sqlite3.Error as e:
+        print(e)
+        connection.rollback()
+    finally:
+        connection.close()
 
 
 if __name__ == '__main__':
